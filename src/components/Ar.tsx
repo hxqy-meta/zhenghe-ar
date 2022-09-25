602f1623fe742aca1be511dcb8f49746189c42fc
uetuluk <uet200@nyu.edu>
2022-09-25 22:16:29 +0800

    feat: remove clutter

--- src/components/Ar.tsx
+++ src/components/Ar.tsx
@@ -1,121 +1,57 @@
+import 'mind-ar/dist/mindar-image.prod.js';
 import 'aframe';
-import 'aframe-look-at-component';
-// import 'aframe-particle-system-component';
-import React from 'react';
-// import ReactDOM from 'react-dom';
-// import '@ar-js-org/ar.js/three.js/build/ar-threex-location-only';
-// import '@ar-js-org/ar.js/aframe/build/aframe-ar';
-import '@ar-js-org/ar.js/aframe/build/aframe-ar-new-location-only';
+import 'mind-ar/dist/mindar-image-aframe.prod.js';
 
-
-// declare global {
-//     namespace JSX {
-//         interface IntrinsicElements {
-//             'a-scene': any,
-//             'a-marker': any,
-//             'a-text': any,
-//             'a-camera': any,
-//             'a-entity': any,
-//             'a-nft': any,
-//             'a-cone': any;
-//         }
-//     }
-// };
-
-// declare var AFRAME: any;
-
-class ARScene extends React.PureComponent {
-    // constructor(props: any) {
-    //     super(props);
-    // }
-    // componentDidMount() {
-    //     document
-    //         .querySelector("a-scene")!
-    //         .addEventListener("loaded", this.onSceneLoad);
-
-    //     // AFRAME.registerComponent("add-number", {
-    //     //     init: function() {
-    //     //     this.el.addEventListener("click", this.onClick.bind(this));
-    //     //     },
-    //     //     onClick: function(e: any) {
-    //     //     console.error("onClick");
-    //     //     }
-    //     // });
-    // }
-    // onSceneLoad = () => {};
-    render () {
-        return (
-            <>
-            <a-scene
-                vr-mode-ui="enabled: false"
-                embedded
-                arjs="sourceType: webcam; videoTexture: true; debugUIEnabled: true;"
-                renderer='antialias: true; alpha: true'
-            >
-
-            <a-text
-                value="This content will always face you."
-                // look-at="[gps-camera]"
-                // scale="120 120 120"
-                gps-entity-place="latitude: 31.1923547; longitude: 121.4376755;"
-            ></a-text>
-            <a-camera gps-camera='gpsMinDistance: 1' rotation-reader> </a-camera>
-            {/* <a-entity camera></a-entity> */}
-            
-            {/* <a-camera gps-new-camera='gpsMinDistance: 5'></a-camera> */}
-            </a-scene>
-            {/* <div id='setloc' style={{ 'position':'absolute', 'left': '10px', 'bottom': '2%', zIndex:'999', backgroundColor: 'blue', color: 'white', padding: '10px'}}>
-                Lat: {}
-                Lon: {position.coords.longitude}
-            </div> */}
-            </>
-            // Marker version
-            // <a-scene arjs="sourceType: webcam; debugUIEnabled: true;">
-            //     <a-marker preset="hiro">
-            //         <a-entity
-            //         position="0 0 0"
-            //         scale="0.05 0.05 0.05"
-            //         gltf-model="./trex/scene.gltf"
-            //         ></a-entity>
-            //     </a-marker>
-            //     <a-entity camera></a-entity>
-            //     {/* <a-camera gps-new-camera='gpsMinDistance: 5'></a-camera> */}
-            // </a-scene>
-            // <a-scene
-            //     vr-mode-ui="enabled: false;"
-            //     renderer="logarithmicDepthBuffer: true;"
-            //     embedded
-            //     arjs="trackingMethod: best; sourceType: webcam;debugUIEnabled: true;"
-            // >
-            //     <a-nft
-            //         type="nft"
-            //         url="./trex/trex-image/trex"
-            //         smooth="true"
-            //         smoothCount="10"
-            //         smoothTolerance=".01"
-            //         smoothThreshold="5"
-            //     >
-            //         <a-text
-            //             value="This content will always face you."
-            //             look-at="[camera]"
-            //             scale="120 120 120"
-            //     ></a-text>
-            //         <a-entity
-            //             gltf-model="./trex/scene.gltf"
-            //             scale="5 5 5"
-            //             position="0.05 0.05 0.05"
-            //         >
-            //         </a-entity>
-            //     </a-nft>
-            //     <a-entity camera></a-entity>
-            // </a-scene>
-
-
-             
-        );
+declare global {
+    namespace JSX {
+        interface IntrinsicElements {
+            'a-scene': any,
+            'a-marker': any,
+            'a-text': any,
+            'a-camera': any,
+            'a-entity': any,
+            'a-nft': any,
+            'a-assets': any,
+            'a-asset-item': any,
+            'a-plane': any,
+            'a-gltf-model': any,
+            'a-cone': any;
+        }
     }
-}
+};
 
-// ReactDOM.render(<VRScene/>, document.querySelector('#sceneContainer'));
+const AR = () => {
+    
+    return (
+        <a-scene 
+            mindar-image="maxTrack: 2; warmupTolerance: 1; filterMinCF:0.1; filterBeta: 10; imageTargetSrc: assets/zhenghe/zhenghe.mind;" 
+            color-space="sRGB" 
+            renderer="colorManagement: true, physicallyCorrectLights" 
+            vr-mode-ui="enabled: false" 
+            device-orientation-permission-ui="enabled: false"
+        >
+            <a-assets>
+                <img id="card" src="assets/card.png" alt="card" />
+                <img id="compass" src="assets/compass/compass.png" alt="compass" />
+                <img id="zhenghe1" src="assets/zhenghe/zhenghe1.png" alt="drawing of zhenghe" />
+                <img id="zhenghe2" src="assets/zhenghe/zhenghe2.jpeg" alt="drawing of zhenghe" />
+                <a-asset-item id="avatarModel" src="assets/scene.gltf"></a-asset-item>
+                <a-asset-item id="horse" src="assets/models/horse/scene.gltf"></a-asset-item>
+                <a-asset-item id="warrior1" src="assets/models/warrior1/scene.gltf"></a-asset-item>
+                <a-asset-item id="warrior2" src="assets/models/warrior2/scene.gltf"></a-asset-item>
+            </a-assets>
 
-export default ARScene;
+            <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
+            <a-entity mindar-image-target="targetIndex: 0">
+                <a-plane src="#zhenghe1" position="0 0 0" height="1" width="1" rotation="0 0 0"></a-plane> 
+                <a-gltf-model rotation="0 0 0" position="0 0 0" scale="10 10 10" src="#warrior1" animation="property: position; to: 0 0.1 0.1; dur: 1000; easing: easeInOutQuad; loop: true; dir: alternate" />
+            </a-entity>
+            <a-entity mindar-image-target="targetIndex: 1">
+                <a-plane src="#zhenghe2" position="0 0 0" height="1" width="1" rotation="0 0 0"></a-plane> 
+                <a-gltf-model rotation="0 0 0" position="0 0 0" scale="10 10 10" src="#horse" animation="property: position; to: 0 0.1 0.1; dur: 1000; easing: easeInOutQuad; loop: true; dir: alternate" />
+            </a-entity>
+        </a-scene>
+    );
+};
+
+export default AR;
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,7 +1,1 @@
 /// <reference types="react-scripts" />
-
-declare module 'aframe-react';
-// declare module 'aframe';
-declare module '@ar-js-org/ar.js/three.js/build/ar-threex.js';
-declare module '@ar-js-org/ar.js/three.js/build/ar-threex-location-only.js'
-declare module 'three';
